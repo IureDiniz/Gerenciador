28befd7ef3136c52a494c0d874e730a6cdd5806f
IureDiniz <iuredinizsoares@gmail.com>
2026-06-05 21:23:43 -0300

    Inicio 3

--- src/app/App.java
+++ src/app/App.java
@@ -8,20 +8,15 @@ import model.User;
 
 public class App {
 
-	public static void main(String[] args) throws SQLException {
-            //System.out.println("oi");
-            
+    public static void main(String[] args) throws SQLException {
+        //System.out.println("oi");
 
-            LoginPage lp = new LoginPage();
-            lp.setVisible(true);
-            //System.out.println("oi");
-                
-                
-            
-            //User user = userDAO.getbyLogin("admin");
-            //System.out.println(user.toString());
+        LoginPage lp = new LoginPage();
+        lp.setVisible(true);
+        //System.out.println("oi");
 
-                
-	}
+        //User user = userDAO.getbyLogin("admin");
+        //System.out.println(user.toString());
+    }
 
 }
