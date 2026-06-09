6cbbedb6310ecaff5ff6673666b0dc9f8d3719e9
GustavoRV08 <gustavorv102@gmail.com>
2026-06-08 21:27:22 -0300

    versão mock para implementação do model
    ainda não está completo

---
+++ src/dao/EquipamentoDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import model.Equipamento;
+/**
+ *
+ * @author Estudante
+ */
+//Versão Mock para implementação do model
+public class EquipamentoDAO {
+    public static Equipamento getEquipamento(int id){
+        return null;
+    }
+    
+    
+}
